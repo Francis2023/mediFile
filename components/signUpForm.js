07c33fc6fec62e0b7c39a59e58e2d99a87c596cb
Francis Ngafua <56661250+Francis2023@users.noreply.github.com>
2020-06-24 15:33:06 -0600

    Add files via upload

---
+++ components/signUpForm.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import {Form,Row,Col,Container,Button} from 'react-bootstrap';
+import dashboard from './dashboard';
+
+class signUpForm extends React.Component {
+    
+   constructor(props) {
+       super(props)
+       this.state = {
+           name: '',
+           email: '',
+           cellphone: '',
+           medicid: '',
+           password: ''
+       }
+   }
+
+   handleChange =  (e) => {
+    this.setState({   [e.target.name]: e.target.value  });
+    
+  }
+  clearForm = (e) => {
+     e.preventDefault();
+     this.setState({ ...this.state, name: '',medicid:'', password:'', email:'', cellphone:''})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.clearForm(e);
+    console.log(this.state)
+    this.props.history.push('/dashboard')
+  
+  }
+
+    render() {
+        return (
+            <Container className="center" >
+            <Form onSubmit={e => this.handleSubmit(e)}>
+              <Form.Group as={Row}  >
+                <Form.Label column sm={2}>
+                  Name
+                </Form.Label>
+                
+                <Col sm={10} >
+                    <Form.Control 
+                      size="lg"
+                      id="nameInput"
+                      name="name"
+                      type="text" 
+                      value={this.state.name}
+                      onChange={e => this.handleChange(e)}
+                    />
+                </Col>
+                </Form.Group>
+                <Form.Group as={Row}  >
+                <Form.Label column sm={2}>
+                  Email
+                </Form.Label>
+                
+                <Col sm={10} >
+                    <Form.Control 
+                      size="lg"
+                      id="emailInput"
+                      name="email"
+                      type="email" 
+                      value={this.state.email}
+                      onChange={e => this.handleChange(e)}
+                    />
+                </Col>
+                </Form.Group>
+                <Form.Group as={Row}  >
+                <Form.Label column sm={2}>
+                  Cellphone
+                </Form.Label>
+                
+                <Col sm={10} >
+                    <Form.Control 
+                      size="lg"
+                      id="cellphoneInput"
+                      name="cellphone"
+                      type="text" 
+                      value={this.state.cellphone}
+                      onChange={e => this.handleChange(e)}
+                    />
+                </Col>
+                </Form.Group>
+                <Form.Group as={Row} >
+                  <Form.Label column sm={2} >
+                    MedicId
+                  </Form.Label>
+                  <Col sm={10}>
+                      <Form.Control 
+                        id="medicIdInput"
+                        name="medicid"
+                        type="text" 
+                        value={this.state.medicid}
+                        onChange={e => this.handleChange(e)}
+                      />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row} >
+                  <Form.Label column sm={2}>
+                    Password
+                  </Form.Label>
+                  <Col sm={10}>
+                      <Form.Control
+                        id="passwordIdInput"
+                        name="password"
+                        type="password" 
+                        value={this.state.password}
+                        onChange={e => this.handleChange(e)}
+                      />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row} >
+                    <Col sm={{span: 2, offset: 2}}>
+                      <Button type="submit" value="Submit"  >
+                         
+                        Submit
+                     
+                      </Button>
+                    </Col>
+                    <Col sm={{span: 2}} >
+                      <Button  onClick={e => this.clearForm(e)} >
+                        Clear
+                      </Button>
+                    </Col>
+                </Form.Group>
+            </Form>
+
+            <Route exact path = "/dashboard" component={dashboard} />
+
+        </Container>
+
+       
+        )
+       
+    }
+}
+
+export default signUpForm;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import'mdbreact/dist/css/mdb.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router} from 'react-router-dom';
+import './fontawesome'
+
+//import './App.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+  
+    <Router >
+    <App />
+    </Router>
+  
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
